97aab6a2d2baf21f441a57df24e9059122598b2c
daoffner <dpoffner@sbcglobal.net>
2016-09-09 16:30:38 -0500

    Made it work on generic sheets
    Made the program adjust the size of the tuple based on the size of the
    spread sheet. By making use of max_column and max_row.

--- ExcelReader.py
+++ ExcelReader.py
@@ -1,9 +1,16 @@
 import openpyxl
+from openpyxl.cell import get_column_letter
 wb = openpyxl.load_workbook('Possibles 3.xlsx', data_only=True)
 sheet = wb.get_sheet_by_name('Sheet1')
-tuple (sheet['A1' : 'R5661'])
+x= get_column_letter(sheet.max_column)
 
-for rowOfCellObjects in sheet['A1' : 'R5661']:
+#print (ord(x))       Prints the ascii value of the max column; needed for testing
+x=str(chr(ord(x)))
+y=str(sheet.max_row)
+#print(y)               Prints the row size of the sheet; needed for testing
+tuple (sheet['A1' : x+y])
+
+for rowOfCellObjects in sheet['A1' : x+y]:
     for cellObj in rowOfCellObjects:
         print(cellObj.coordinate, cellObj.value)
 
