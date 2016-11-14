5160b1a251210d66200470c67e9b7baa39a3efc0
daoffner <dpoffner@sbcglobal.net>
2016-11-13 22:36:26 -0600

    The program runs and kills fo.
    Also implemented it saving the image sets that contain the possibles in
    a textfile. Removed runFo.java it wasn't needed.

--- ExcelReader.py
+++ ExcelReader.py
@@ -4,19 +4,11 @@ import os
 import threading
 import time
 import glob
-import signal
-#from openpyxl.cell import get_column_letter
+
 def find_orb():
-    #print(threadName)
-    os.system("javac runFo.java")
-    os.system("java runFo")
-    #os.system('./fo mpc.txt')
-def delay(threadName, wait):
-    print(threadName)
-    time.sleep(wait)
-    os.system('\x03')
-    os.remove('MPCORB.DAT')
-    os.rename('mpc_fmt.txt','MPCORB.DAT')
+    os.system("./fo mpc.txt")
+    #os.system("javac runFo.java")
+    #os.system("java runFo")
 
 wb = openpyxl.load_workbook('Possibles 3.xlsx', data_only=True)
 sheet = wb.get_sheet_by_name('Sheet1')
@@ -37,17 +29,18 @@ tuple (sheet['A1' : x+y])
 path= 'MPC'
 dirs = os.listdir("MPC")
 #dirs = os.listdir(os.path.dirname(path))
-for file in dirs:
-   print (file)
+#for file in dirs:
+#   print (file)
 mpcArray= []
+imageArray= []
 i=0
 #New code to test
 for filename in glob.glob(os.path.join(path, '*.txt')):
-    print(filename)
+    #print(filename)
     r= open(filename, "r")
     for line in r.readlines():
         mpcArray.append(line.strip())
-        print(mpcArray[i])
+        #print(mpcArray[i])
         i=i+1
 #    i=i+1
     r.close()
@@ -62,34 +55,40 @@ for filename in glob.glob(os.path.join(path, '*.txt')):
 #    i=i+1
 #r.close()
 f= open("mpc.txt", "w")
+g= open("image.txt", "w")
+k= 0
+size=0
 for j in range(int(y)):
     for i in range(len(mpcArray)):
         name= sheet['A'+str(j+1)].value
+        image= sheet['F'+str(j+1)].value
         #print(re.search(name, mpcArray[i], flags=0))
         regex= re.search(name, mpcArray[i], flags=0)        #Searches for Objects that are in both files
         if regex:
             f.write("     "+mpcArray[i]+'\n')               #Writes a new MPC Report need to create an orbit
+            size=size+1
+            if(len(imageArray) != 0):
+                if(imageArray[k] != image):
+                    k=k+1
+                    imageArray.append(image)
+                    g.write(image+'\n')
+            else:
+                    imageArray.append(image)
+                    g.write(image+'\n')
+            #k=k+1
+            #print(imageArray)
             #print(type(mpcArray[i]))
             mpcArray[i]= "0"
             #print(regex)
-#f= open("mpc.txt", "w")
-#for k in range(len(mpcArray)):
-#    f.write("     "+mpcArray[k]+'\n')
 
+print(size)
 f.close()
 t = threading.Thread(target= find_orb)
 t.daemon= True
 t.start()
-#thread.start_new_thread(find_orb, ("Thread-1", ))
-#thread.start_new_thread(delay, ("Thread-2" , 20, ))
-#os.system('./fo mpc.txt')
-#os.remove('MPCORB.DAT')
-#os.rename('mpc_fmt.txt','MPCORB.DAT'
-time.sleep(20)
-#c= chr(3)
-#os.system(c)
-#t.join()
-#t._stop.set()
+
+time.sleep(size/8)
+os.system("killall -9 fo")
 os.remove('MPCORB.DAT')
 os.rename('mpc_fmt.txt','MPCORB.DAT')
 os.system('cp -u MPCORB.DAT /mnt/c/Astrometrica/Catalogs')
