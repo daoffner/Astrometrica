100a7199e9bf326e2ffaef094c1424fa6a0bc541
daoffner <dpoffner@sbcglobal.net>
2017-05-26 11:55:31 -0400

    Fixed an error and cleaned up the directory of unecessary files.

--- ExcelMPC.py
+++ ExcelMPC.py
@@ -37,10 +37,11 @@ k= 0
 size=0
 
 for j in range(int(y)):
+    for i in range(len(mpcArray)):
         name= sheet['A'+str(j+1)].value
         image= sheet['F'+str(j+1)].value
         #print(re.search(name, mpcArray[i], flags=0))
-       regex= re.search(name, mpcArray[i], flags=0)        #Searches for Objects that are in both files
+        regex= re.search(name, mpcArray[i], flags=0)        #Searches for Objects that are in both files
         if regex:
             f.write("     "+mpcArray[i]+'\n')               #Writes a new MPC Report need to create an orbit
             size=size+1
@@ -53,7 +54,7 @@ for j in range(int(y)):
                     imageArray.append(image)
                     g.write(image+'\n')
             mpcArray[i]= "0"
-
+print(size)
 f.close()
 g.close()
 t = threading.Thread(target= find_orb)
